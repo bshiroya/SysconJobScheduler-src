0cd86da16ecaeb7dd4fc0777b3a3e134fca28538
vimalgupta1980 <guptavimal1980@gmail.com>
2014-04-10 17:04:48 +0530

    SysconJobScheduler ver 1.1.0 final
    SysconJobScheduler ver 1.1.0 final with updated batch files
    Added Export billing updates job and work order import job

--- JobSchedulerService/ServiceInstall.bat
+++ JobSchedulerService/ServiceInstall.bat
@@ -1,3 +1,5 @@
+set path=%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+
 installutil /i JobSchedulerService.exe
 
 sc start JobSchedulerService
--- JobSchedulerService/ServiceUnInstall.bat
+++ JobSchedulerService/ServiceUnInstall.bat
@@ -1,3 +1,5 @@
+set path=%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+
 sc stop JobSchedulerService
 
 installutil /u JobSchedulerService.exe
