f5d118d01458c273055dd7e6ae2dae3a69ce18d3
Vimal Gupta <guptavimal1980@gmail.com>
2014-10-09 00:19:41 +0530

    SysconJobScheduler-1.4.2
    Changed product code. Created new setup containing no plugin dlls by
    default.

--- JobSchedulerUI/JobScheduler.cs
+++ JobSchedulerUI/JobScheduler.cs
@@ -131,10 +131,10 @@ namespace Syscon.JobSchedulerUI
         private void JobScheduler_Load(object sender, EventArgs e)
         {
             // resets it everytime it is run so that the user can't just change to a product they already have a license for
-            Env.SetConfigVar("product_id", 178507);
+            Env.SetConfigVar("product_id", 337264);
 
             var product_id = Env.GetConfigVar("product_id", 0, false);
-            var product_version = "1.4.1.0";
+            var product_version = "1.4.2.0";
             bool require_login = false;
 
             if (!_loaded)
@@ -451,7 +451,7 @@ namespace Syscon.JobSchedulerUI
         private void activateToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var product_id = Env.GetConfigVar("product_id", 0, false);
-            var product_version = Env.GetConfigVar("product_version", "1.4.1.0", false);
+            var product_version = Env.GetConfigVar("product_version", "1.4.2.0", false);
 
             var frm = new SysconCommon.Protection.ProtectionPlusOnlineActivationForm(product_id, product_version);
             frm.ShowDialog();
